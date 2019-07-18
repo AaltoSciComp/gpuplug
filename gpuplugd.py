cbb21a8404eb96da0b79375802c13e24f3035eff
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-07-18 16:34:24 +0300

    gpuplugd: Make the container servers threaded

--- gpuplugd.py
+++ gpuplugd.py
@@ -2,6 +2,7 @@
 
 import socketserver
 import os
+import threading
 
 CTL_SOCKET_PATH = '/tmp/gpuplug-ctl'
 CNT_SOCKET_BASE_PATH = '/tmp/gpuplug-'
@@ -15,19 +16,27 @@ class ContainerSocket(socketserver.BaseRequestHandler):
     def handle(self):
         print('handle container')
 
+class ThreadedUnixServer(socketserver.ThreadingMixIn,
+                         socketserver.UnixStreamServer):
+    pass
+
 class ControlSocket(socketserver.BaseRequestHandler):
     def handle(self):
         self.path = container_socket_path(len(container_servers))
-        server = socketserver.UnixStreamServer(self.path, ContainerSocket)
-        container_servers.append(server)
+        server = ThreadedUnixServer(self.path, ContainerSocket)
+        self.thread = threading.Thread(target = server.serve_forever)
+        self.thread.daemon = True
+        self.thread.start()
+        container_servers.append(self)
 
         self.request.sendall(str.encode(self.path, 'ascii'))
+        print('Create container socket: {}'.format(self.path))
 
     def get_path(self):
         return self.path
 
 if __name__ == '__main__':
-    ctl_server = socketserver.UnixStreamServer(CTL_SOCKET_PATH, ControlSocket)
+    ctl_server = ThreadedUnixServer(CTL_SOCKET_PATH, ControlSocket)
     try:
         ctl_server.serve_forever()
     except KeyboardInterrupt:
@@ -35,5 +44,6 @@ if __name__ == '__main__':
     finally:
         os.unlink(CTL_SOCKET_PATH)
         for i in range(0, len(container_servers)):
+            container_servers[i].server.shutdown()
             os.unlink(container_socket_path(i))
         print('bye!')
