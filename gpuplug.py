afb8c0fe7ab849d7c0a8a74f3eb75bb6ee653dc4
Aapo Vienamo <aapo.vienamo@iki.fi>
2019-07-19 14:46:39 +0300

    gpuplug: Factor out the request communication

--- gpuplug.py
+++ gpuplug.py
@@ -11,23 +11,27 @@ def get_container_id():
     f.close()
     return l.split('/')[2]
 
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: gpuplugc.py get|put")
-        return
+def gpu_req(verb):
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     sock.connect(SOCKET_PATH)
-    sock.sendall(str.encode(sys.argv[1] + ':' + get_container_id() + '\n',
-                            'ascii'))
+    sock.sendall(str.encode(verb + ':' + get_container_id() + '\n', 'ascii'))
     msg = ''
     while True:
         b = sock.recv(1)
         if len(b) == 0:
             break
         msg += b.decode('ascii')
+
     sock.close()
-    print(msg)
-    if msg != 'Ok':
+    return msg
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: gpuplugc.py get|put")
+        return
+    ret_msg = gpu_req(sys.argv[1])
+    print(ret_msg)
+    if ret_msg != 'Ok':
         exit(-1)
 
 if __name__ == '__main__':
